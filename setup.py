e9d4b4bc0557a4e815f98c076a20b5af3fe46e93
MilagrosMarin <milmarina02@gmail.com>
2023-11-10 17:14:22 -0600

    add DevContainers

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from os import path
+from setuptools import find_packages, setup
+import urllib.request
+
+pkg_name = "element_moseq"
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), "r") as f:
+    long_description = f.read()
+
+with open(path.join(here, pkg_name, "version.py")) as f:
+    exec(f.read())
+
+setup(
+    name=pkg_name.replace("_", "-"),
+    version=__version__,  # noqa: F821
+    description="Keypoint Moseq DataJoint Element",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="DataJoint",
+    author_email="info@datajoint.com",
+    license="MIT",
+    url=f'https://github.com/datajoint/{pkg_name.replace("_", "-")}',
+    keywords="neuroscience keypoint-moseq science datajoint",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    scripts=[],
+    install_requires=[
+        "datajoint>=0.13.0",
+        "ipykernel>=6.0.1",
+        "ipywidgets",
+        "plotly",
+    ],
+    extras_require={
+        "moseq": ["moseq @ git+https://github.com/dattalab/keypoint-moseq.git"],
+        "elements": [
+            "element-animal>=0.1.8",
+            "element-event>=0.2.3",
+            "element-interface>=0.6.0",
+            "element-lab>=0.3.0",
+            "element-session>=0.1.5",
+        ],
+    },
+)
