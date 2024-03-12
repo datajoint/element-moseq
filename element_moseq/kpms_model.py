aa151c21671e635591ecc006df8a98c5c8b808c4
MilagrosMarin <milmarina02@gmail.com>
2024-03-12 18:51:58 +0100

    Update `Prefitting` computation logic

--- element_moseq/kpms_model.py
+++ element_moseq/kpms_model.py
@@ -3,6 +3,7 @@ from typing import Optional
 import pickle
 import inspect
 import importlib
+import os
 from pathlib import Path
 from element_interface.utils import find_full_path
 from .readers.kpms_reader import load_dj_config, generate_dj_config
@@ -11,6 +12,8 @@ from keypoint_moseq import (
     format_data,
     load_pca,
     init_model,
+    update_hypparams,
+    fit_model
 )
 from element_moseq.kpms_pca import PCATask
 
@@ -112,8 +115,8 @@ class PreFittingTask(dj.Manual):
     kappa                    : int
     num_iterations           : int
     ---
-    model_name              : varchar(20)
     task_mode='load'        : enum('load', 'trigger')  # 'load': load computed analysis results, 'trigger': trigger computation
+    prefitting_desc=''      : varchar(1000)
     """
 
 
@@ -122,29 +125,76 @@ class PreFitting(dj.Computed):
     definition = """
     -> PreFittingTask
     ---
-    model                   : longblob
-    suggested_laten_dim: int
-    suggested_kappa: int
-    suggested_num_iterations: int
+    model_name              : varchar(255)
     """
 
     def make(self, key):
-        # latent_dim = (PreFittingTask & key).fetch1("latent_dim")
-        # config = load_config()
-        # check_config_validity(config)
-        # config.update(dict(latent_dim = int(latent_dim)))
+        output_dir = (PCATask & key).fetch1("output_dir")
+        task_mode, latent_dim, kappa, num_iterations = (PreFittingTask & key).fetch1("task_mode","latent_dim","kappa","num_iterations")
 
-        # model = kpms.init_model(data, pca=pca, **config())
-        # model = kpms.update_hypparams(model, kappa=kappa)
-        # model, model_name = kpms.fit_model(model, data, metadata, project_dir,ar_only=True, num_iters=num_ar_iters)
-        return
+        if task_mode == "trigger":
+            # Update `dj_config.yml` with the latent dimensions 
+            config = load_dj_config(output_dir,check_if_valid=True, build_indexes=True)
+            config.update(dict(
+                            latent_dim=int(latent_dim),
+                            kappa=int(kappa)
+                            )
+                        )
+            generate_dj_config(output_dir, **config)
+            
+            # Load the pca model and the data 
+            pca = load_pca(output_dir)
 
+            data_path = os.path.join(output_dir, 'data.pkl')
+            with open(data_path, 'rb') as data_file:
+                data = pickle.load(data_file)
 
+            metadata_path = os.path.join(output_dir, 'metadata.pkl')
+            with open(metadata_path, 'rb') as data_file:
+                metadata = pickle.load(data_file)
+                
+            # Initialize the model 
+            model = init_model(data=data, metadata=metadata, pca=pca, **config)
+
+            # update with chosen kappa
+            model = update_hypparams(model, kappa=float(kappa))
+            #model has jaxlib.xla_extension.Array, so it cannot be stored as longblob.
+            
+            # # fit AR-HMM model:
+            # - generates a name for the model and a corresponding directory in project_dir
+            # - saves a checkpoint every 25 iterations from which fitting can be restarted
+            # - plots the progress of fitting every 25 iterations, including
+                # 1. the distributions of syllable frequencies and durations for the most recent iteration
+                # 2. the change in median syllable duration across fitting iterations
+                # 3. a sample of the syllable sequence across iterations in a random window
+            model, model_name = fit_model(model, 
+                                        data, 
+                                        metadata, 
+                                        output_dir,
+                                        ar_only=True, 
+                                        num_iters=num_iterations
+                                        )
+
+            model_path = os.path.join(output_dir + "/" + model_name + "/" + "model.pkl")
+            with open(model_path, "wb") as data_file:
+                pickle.dump(model, data_file)
+
+            self.insert1({**key,
+                        "model_name":model_name})
+        
+        elif task_mode == "load":
+            pass
+        
 @schema
 class FullFittingTask(dj.Manual):
+    """
+
+    The full model will generally require a lower value of kappa to yield the same target syllable durations.
+    """
+
     definition = """
     -> kpms_pca.PCAFitting
-    latent_dim               : int
+    latent_dim              : int
     kappa                   : int
     num_iterations          : int
     ---
@@ -153,13 +203,14 @@ class FullFittingTask(dj.Manual):
     """
 
 
-# @schema
-# class FullFitting(dj.Computed):
-#     definition = """
-#     -> FullFittingTask
-#     ---
-#     model                   : longblob
-#     """
+@schema
+class FullFitting(dj.Computed):
+    definition = """
+    -> FullFittingTask
+    ---
+    model_name              : varchar(255)
+    model                   : longblob
+    """
 
 # def make(self, key):
 # task_mode == "trigger"
