0390ee9a8cab6e2ee430a762b208b244a2723461
MilagrosMarin <milmarina02@gmail.com>
2025-10-24 00:46:52 +0200

    update(tutorial_pipeline): to remove `moseq_report`

--- notebooks/tutorial_pipeline.py
+++ notebooks/tutorial_pipeline.py
@@ -4,7 +4,7 @@ from collections import abc
 from element_lab import lab
 from element_animal import subject
 from element_session import session_with_datetime as session
-from element_moseq import moseq_train, moseq_infer, moseq_report
+from element_moseq import moseq_train, moseq_infer
 
 from element_animal.subject import Subject
 from element_lab.lab import Source, Lab, Protocol, User, Project
@@ -44,7 +44,6 @@ __all__ = [
     "session",
     "moseq_train",
     "moseq_infer",
-    "moseq_report",
     "Device",
 ]
 
@@ -88,4 +87,3 @@ class Device(dj.Lookup):
 
 moseq_train.activate(db_prefix + "moseq_train", linking_module=__name__)
 moseq_infer.activate(db_prefix + "moseq_infer", linking_module=__name__)
-moseq_report.activate(db_prefix + "moseq_report", linking_module=__name__)
